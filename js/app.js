54c13cf35f7876a3abbcfbeb2af705e019f053f5
martinacruz <cruz.s.martin@gmail.com>
2020-10-12 01:48:15 -0700

    still very bad at commiting but its done

--- js/app.js
+++ js/app.js
@@ -1,126 +1,152 @@
 console.log('Hell Week #1')
 
-// //James Tip - Refrence Poke-a-Dot ALOT
+//James Tip - Refrence Poke-a-Dot ALOT
 
-// //function to-do list:
-// //set random boxes ( 2 rows, 6 boxes each)
-// //comparing clicked cards
+//function to-do list:
+//set random boxes ( 2 rows, 6 boxes each)
+//comparing clicked cards
 // prevent clicked cards from being clicked again. const boolean?
-// //increase level (add mistake, add row,)
+//increase level (add mistake, add row,)
 
-// //click, rotate, show, set, click, rotate, show, set compare, reset position, (set if same), repeate until finished
+//click, rotate, show, set, click, rotate, show, set compare, reset position, (set if same), repeate until finished
 
-// create an array to store all the card information. build a for loop that will go through these cards and set them on the board.
-// Use cads.name to find matches in your if/else statement
-// cards = [ 
-//     {
-//         name: 'Magic',
-//         src: './images/magiccard.png'
-//     },
-//     {
-//         name: 'Pokemon',
-//         src: './images/pokemoncard.jpg'
-//     },
-//     {
-//         name: 'Fate/Go',
-//         src: './images/fategocard.png'
-//     },
-//     {
-//         name: 'Among Us',
-//         src: './images/redkindasus.jpg'
-//     },
-//     {
-//         name: 'Yugioh',
-//         src: './images/yugiohcard.jpg'
-//     },
-//     {
-//         name: 'UNO',
-//         src: './images/unocard.jpg'
-//     }
-// ]
 
 //need variables?
 let timer = 30;
 let lvl = 1;
 let mistakes = 0;
-let wasFlipped = false
-let firstFlipped = ''
-let secondFlipped = ''
+let wasFlipped = false //has card been flipped?
+let selectedCards = []
+let setDeck = []
+let paired = []
 
-const cards = document.querySelectorAll('.cards');
+const gameCards = [{
+    card: 'among us',
+    src: './images/redkindasus.jpg'
+},
+{
+    card: 'fatego',
+    src: './card/fategocard.pgn'
+},
+{
+    card: 'magic',
+    src: './images/magiccard.png'
+},
+{
+    card: 'pokemon',
+    src: './images/pokemoncard.jpg'
+},
+{
+    card: 'uno',
+    src: './images/unocard.jpg'
+},
+{
+    card: 'yugioh',
+    src: './images/yugiohcard.jpg'
+}
+]
 
-for (let i=0; i<cards.length; i++) {
-    cards[i].addEventListener('click', flipCard)
+
+//use new array to set the gamecards
+//this should run once the page is open
+//set attributes same as old version to not double work on the css
+
+//--------------create game----------------
+
+//access the 'game' div to append childs to
+const gameLayout = document.querySelector('#game')
+
+function setGame() {
+    //set up the layed out cards w/ for loop like before 
+    for (let i=0; i<gameCards.length; i++){ //double of gameCards array because sets of 2 for each object
+    //create the tags
+    const firstSet = document.createElement('img')
+    firstSet.setAttribute('class', 'cards')
+    //give it the 'cardBack' source
+    firstSet.setAttribute('src', './images/cardBack.png')
+    //set id# to comapre it later and not fall into same issue
+    firstSet.setAttribute('id', i)
+    gameLayout.appendChild(firstSet)
+    setDeck.push(firstSet)
+    }
+    // if we want to comapre using 'id' we need to set it 2 different times or else #'s will go from 0 to 11 and w/ no set match.
+    for (let i=0; i<gameCards.length; i++){
+        const secondSet = document.createElement('img')
+        secondSet.setAttribute('class', 'cards')
+        secondSet.setAttribute('src', './images/cardBack.png')
+        secondSet.setAttribute('id', i)
+        gameLayout.appendChild(secondSet)
+        setDeck.push(secondSet)
+        }
+    //console.log(setDeck)
+    setClick()
+    shuffle()
 }
 
-randomize()
-
-//--------------- flipping function ------------
-
-function flipCard() {
-    //add class to clicked cards to style in css
-    this.classList.add('flipped')
-    //prevent card from being flipped after first click
-    if (!wasFlipped){
-        wasFlipped = true
-        firstFlipped = this
-        console.log(firstFlipped)
-        let firstDiv = this.getElementByClass('cards flipped')
-        console.log(firstDiv)
-        //firstSrc = firstDiv.getElementByClass('frontFace').src
-        //console.log(firstSrc)
-    } else {
-        wasFlipped = false
-        secondFlipped = this
-        console.log(firstFlipped, secondFlipped)
-    }
-
-    if (firstFlipped === secondFlipped ) {
-        //keep flipped
-        firstFlipped.removeEventListener('click', flipCard)
-        secondFlipped.removeEventListener('click', flipCard)
-        console.log('this is a match')
-    } else {
-        //flip back down
-
-        //mistake counter goes up
-        this.mistakes++
-        document.querySelector('#mistakes').innerHTML = `Mistakes: ${this.mistakes}/3`
-        
-        // if (mistakes === 3) {
-        //     alert('That`s 3 mistakes! GAME OVER!)
-        // }
-    }
-
+//set click event listeners to all .cards
+function setClick () {
+    const cards = document.querySelectorAll('.cards');
+        for (let i=0; i<cards.length; i++) {
+        cards[i].addEventListener('click', cardFlip)
 }
-//ISSUES:
-//flipCard function keep showing true if using 'this.classList !== "cards flipped"'
-//create two variables for first and second flipped?
+}
 
-//----------------- Randomize function ---------------
 
-function randomize() {
-    //select ONLY the hidden cards (.fontFace)
-    let content = document.querySelectorAll('.frontFace')
-    // ceate array to loop through ./images
-    let src = ['./images/unocard.jpg', './images/yugiohcard.jpg', './images/redkindasus.jpg', './images/fategocard.png', './images/pokemoncard.jpg', './images/magiccard.png', './images/unocard.jpg', './images/yugiohcard.jpg', './images/redkindasus.jpg', './images/fategocard.png', './images/pokemoncard.jpg', './images/magiccard.png']
-//I am getting undefined for some reason while for loop runs
-//YOU HAVE TO DOUBLE THEM!!!
+//----------Shuffle Function----------
 
-    //loop though src
-    for(let i=src.length-1; i >= 0; i--){
+//Take setDeck and move id's around
+function shuffle () {
+    for(let i=setDeck.length-1; i >= 0; i--){
         let j = Math.floor(Math.random() * i)
-        let temp = src[i]
-        src[i] = src[j]
-        src[j] = temp
-    }
-    //loop through content(img's) and set their src randomly
-    for (let i=0; i<content.length; i++) {
-        content[i].src = src[i]
+        let temp = setDeck[i]
+        setDeck[i] = setDeck[j]
+        setDeck[j] = temp
     }
 }
 
-//-------------- compare function ------------
-function compare() {
 
+//----------Flipping the cards-----------
+
+// all in card flip function
+//  - get id #
+//  - set a new src
+//  - push to selected cards
+
+function cardFlip () {
+    //this.classList.add('flipped') //give class to keep CSS
+    const cardId = this.getAttribute('Id') //get id # to set bottom card/match
+    console.log(cardId)
+    //need id of getId to find matching id from array (element position)
+    this.setAttribute('src', `./images/card${cardId}.png`)
+    selectedCards.push(this)
+    if (selectedCards.length == 2) {
+        setTimeout(checkMatch, 500) //fire checkMatch with short delay 
+        }
+        if (paired.length === 12) {
+            alert('You won the game!')
+        }
 }
+
+// ------------- Check Mach--------
+
+//  - use card id #'s to match
+//  - compare selected cards (fire after second selection)
+//  - do things if == (set and leave up)
+//  - do things if !==  (remove from selectedCards and flip back down)
+function checkMatch () {
+    if (selectedCards[0].getAttribute('id') == selectedCards[1].getAttribute('id')) {
+        console.log('found a pair')
+        paired.push(selectedCards[0]) //push into new array to use in win argument
+        paired.push(selectedCards[1])
+        selectedCards.shift(); //reset array
+        selectedCards.shift();
+        } else {
+        console.log('this is not a match')
+        selectedCards[0].setAttribute('src', './images/cardBack.png'); //set cards back down
+        selectedCards[1].setAttribute('src', './images/cardBack.png');
+        selectedCards.shift(); //remove from 'selectedCards' array
+        selectedCards.shift();
+        }
+}
+
+setGame()
